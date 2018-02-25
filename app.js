5bf19b0dce4db7b4eab75c7553c11a15e85281f8
Lynn Li <yuhua.li@inveno.com>
2018-02-25 16:42:41 +0800

    [feature]: implementing HTTP get

--- app.js
+++ app.js
@@ -4,10 +4,20 @@ var app = express();
 
 var port = process.env.PORT || 3888;
 
+var bookRouter = express.Router();
+bookRouter.route('/Books')
+    .get(function(req, res) {
+        var responseJson = {
+            hello: "This is my api"
+        };
+        res.json(responseJson);
+    });
+app.use('/api', bookRouter);
+
 app.get('/', function(c, res) {
     res.send('Welcome to my API');
 });
 
 app.listen(port, function() {
-    console.log('Running on PORT ' + port);
+    console.log('Gulp is running my app on PORT ' + port);
 });
