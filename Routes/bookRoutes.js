184bb96d08a45f51e9eb3c648bf91d7a2ac07832
Lynn Li <yuhua.li@inveno.com>
2018-02-26 10:53:43 +0800

    [feature]: add some interfaces for book module, for example: get post put patch delete

--- Routes/bookRoutes.js
+++ Routes/bookRoutes.js
@@ -2,47 +2,63 @@ var express = require('express');
 
 var routes = function(Book) {
     var bookRouter = express.Router();
+    var bookController = require('../Controllers/bookController')(Book);
 
     bookRouter.route('/')
-        .post(function(req,res) {
-            var book = new Book(req.body);
-            
-            book.save();
-            res.status(201).send(book);
-        })
-        .get(function(req, res) {
-            var query = req.query;
+        .post(bookController.post)
+        .get(bookController.get);
 
-            Book.find(query, function(err, books) {
-                if(err) {
-                    res.status(500).send(err);
-                } else {
-                    res.json(books);
-                }
-            });
+    bookRouter.use('/:bookId', function(req, res, next) {
+        Book.findById(req.params.bookId, function(err, book) {
+            if(err) {
+                res.status(500).send(err);
+            } else if(book) {
+                req.book = book;
+                next();
+            } else {
+                res.status(404).send('no book found');
+            }
         });
-
+    });
     bookRouter.route('/:bookId')
         .get(function(req, res) {
-            Book.findById(req.params.bookId, function(err, book) {
+            res.json(req.book);
+        })
+        .put(function(req, res) {
+            req.book.title = req.body.title;
+            req.book.genre = req.body.genre;
+            req.book.author = req.body.author;
+            req.book.read = req.body.read;
+            req.book.save(function(err) {
                 if(err) {
                     res.status(500).send(err);
                 } else {
-                    res.json(book);
+                    res.json(req.book);
                 }
             });
         })
-        .put(function(req, res) {
-            Book.findById(req.params.bookId, function(err, book) {
+        .patch(function(req, res) {
+            if(req.body._id) {
+                delete req.body._id;
+            }
+
+            for(var p in req.body) {
+                req.book[p] = req.body[p];
+            }
+            req.book.save(function(err) {
                 if(err) {
                     res.status(500).send(err);
                 } else {
-                    book.title = req.body.title;
-                    book.genre = req.body.genre;
-                    book.author = req.body.author;
-                    book.read = req.body.read;
-                    book.save();
-                    res.json(book);
+                    res.json(req.book);
+                }
+            });
+        })
+        .delete(function(req, res) {
+            req.book.remove(function(err) {
+                if(err) {
+                    res.status(500).send(err);
+                } else {
+                    res.status(204).send('Removed');
                 }
             });
         });
